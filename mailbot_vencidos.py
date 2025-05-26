4cd90ae2de793db7b8889620fb4fb7ab35c5fc00
Matheus <bvlxtelli@outlook.com>
2025-05-26 17:50:06 -0300

    Refactor email recipient assignment for clarity

--- mailbot_vencidos.py
+++ mailbot_vencidos.py
@@ -198,8 +198,7 @@ def enviar_vencidos():
 
     if anexos:
         
-        destinatario = ",".join(['jocelene.paes@bistek.com.br','uzias.souza@bistek.com.br','central.cbm@bistek.com.br'])#['matheus.moura@bistek.com.br'])#['matheus.moura@bistek.com.br'])#
-
+        destinatario = ",".join(['jocelene.paes@bistek.com.br','uzias.souza@bistek.com.br','central.cbm@bistek.com.br'])#['matheus.moura@bistek.com.br'])
         print(f"[ {pd.Timestamp.now()} ] Enviando e-mail para {destinatario}...")
 
         enviar_email(
