d2685a63d180d82dc42c580984a1d4fbec0720a2
Matheus <bvlxtelli@outlook.com>
2025-05-29 18:39:15 -0300

    Add new script for generating reports in mailbot_quebras_duplicadas.py

---
+++ mailbot_quebras_duplicadas.py
@@ -0,0 +1,22 @@
+from my_lib import *
+from mailbot import *
+from modules import quebra_conhecida, enviar_email_com_tabela
+
+print(f"[ {pd.Timestamp.now()} ] Iniciando aplicação...")
+
+def dpc():
+
+    print(f"[ {pd.Timestamp.now()} ] Buscando relatório no Thincake...")
+
+    x = quebra_conhecida()
+
+    print(f"[ {pd.Timestamp.now()} ] Relatório criado")
+
+    if dia_da_semana != 'Segunda':
+        x = x[(x['DATA_FATURAMENTO'] == ontem)]
+    else:
+        x = x[(x['DATA_FATURAMENTO'] < hoje) & (x['DATA_FATURAMENTO'] >= sexta)]
+    
+    x['CONT_SES'] = x.groupby(['CODIGO', 'DESCRICAO', 'LOJA'])['CODIGO'].transform('count')
+
+    return x
