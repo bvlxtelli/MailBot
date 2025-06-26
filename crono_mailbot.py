5f3bad767fe3ca67eb39f98a302019bff3b9634a
Matheus <bvlxtelli@outlook.com>
2025-06-26 14:45:19 -0300

    Refactor imports in crono_mailbot.py to use bots module

--- crono_mailbot.py
+++ crono_mailbot.py
@@ -4,20 +4,7 @@ from mailbot import *
 import warnings
 warnings.simplefilter(action='ignore', category=pd.errors.SettingWithCopyWarning)
 
-from mailbot_agendas_semanais import enviar_agendas
-from mailbot_agendas_semanais_vlr import enviar_agendas_por_valor
-from mailbot_bolos import enviar_bolos
-from mailbot_maiores_qbc_flv import enviar_maiores_quebras
-from mailbot_pereciveis import enviar_pereciveis
-from mailbot_sacolas import enviar_sacolas
-from mailbot_sem_venda import enviar_sem_venda
-from mailbot_suspeitos_pendentes import enviar_suspeitos_pendentes
-from mailbot_vencidos import enviar_vencidos
-from mailbot_qbc_troca_665 import enviar_qbc_troca_665
-from mailbot_quebra_campanha import enviar_quebra_campanha
-from mailbot_divergencias_altas import enviar_divergencias_altas
-from mailbot_duplicidades_gerencial import enviar_duplicidades_gerenciais
-from mailbot_saldo_maior_que_ep import enviar_saldo_maior_que_ep
+from bots import *
 
 if dia_da_semana in ['Segunda', 'Quarta', 'Sexta']:
     
